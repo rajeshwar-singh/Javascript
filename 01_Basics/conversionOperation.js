7b4c63101933f22cae4d00e8b7d72b66ae5c3f76
Rajeshwar  Singh <55869135+rajeshwar-singh@users.noreply.github.com>
2024-02-22 11:08:12 +0000

    conversion

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,1 +1,26 @@
-let score = 35
+let score = "44.5"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let value = Number(score)
+console.log(typeof value)
+console.log(typeof(value))
+console.log(value)
+
+//NaN example : Not a number
+
+let num = "85ds"
+console.log(typeof(num))
+let convertToNum = Number(num)
+console.log(typeof(convertToNum))
+console.log(convertToNum)
+
+
+//conversion into boolean
+//1-> true, 0->false, ""->false, "ygHDJKF"->ture
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log("Value : ", booleanLoggedIn)
+console.log(typeof booleanLoggedIn)
+
