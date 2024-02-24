47fc57e51f7ca3bef9215cbbcb77971b56d0e42c
Rajeshwar  Singh <55869135+rajeshwar-singh@users.noreply.github.com>
2024-02-24 16:12:29 +0000

    conversion and operations

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -24,3 +24,46 @@ let booleanLoggedIn = Boolean(isLoggedIn)
 console.log("Value : ", booleanLoggedIn)
 console.log(typeof booleanLoggedIn)
 
+//checking for operation
+console.log(undefined>=0)
+
+//usage of symbol
+const id = Symbol('e93')
+const ids  = Symbol('e93')
+console.log(id)
+console.log(ids)
+console.log(id===ids)
+
+//Example how can we use bigInt
+const bigNumber = 40938488439433489403285848084n
+console.log("example of big int : ",bigNumber)
+
+
+//Data types are of two types:
+    /* 1) Primitive Type -> String, Number, Boolean, Undefined, Symbol, Null and BigInt
+       2) Non-Primitive Type or refernce type-> Array, Object and function
+    */ 
+
+//reference types
+
+//array
+const heros = ['Hanuman ji', 'Angad']
+heros.forEach(element => {
+    console.log("Hero : ",element)
+});
+
+//object
+let objects = {
+    name: "Rajeshwar",
+    age: 25,
+    occupation: "Software Engineer"
+}
+
+console.log(objects.name,": ", objects.occupation)
+
+//function
+const myFunction = func()
+{
+    console.log("Function")
+}
+
